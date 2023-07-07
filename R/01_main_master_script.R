b8d8ef5c854be425ab10f9c3c712f7a338086d66
Adrian Minnig <adrian.minnig96@gmail.com>
2023-07-07 16:44:11 +0200

    creating main script

---
+++ R/01_main_master_script.R
@@ -0,0 +1,10 @@
+# This is the main / master script for the project Antibiotikaverbrauch
+# It is used to source the other scripts
+# Author: Adrian Minnig
+
+setwd("~/GitHub/Projekt_Antibiotikaverbrauch")
+install.packages(c("dplyr", "tidyverse", "lubridate"))
+
+library("dplyr")
+library("tidyverse")
+library("lubridate")
