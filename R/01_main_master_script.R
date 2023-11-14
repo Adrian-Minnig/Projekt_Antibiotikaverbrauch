8cfe940585fc9d31064ff46ba0c28f1dd4c98993
Adrian Minnig <adrian.minnig96@gmail.com>
2023-11-14 15:26:35 +0100

    preparing main script
    selecting and loading necessry packages

--- R/01_main_master_script.R
+++ R/01_main_master_script.R
@@ -3,9 +3,18 @@
 # Author: Adrian Minnig
 
 setwd("~/GitHub/Projekt_Antibiotikaverbrauch")
-install.packages(c("dplyr", "tidyverse", "lubridate"))
+install.packages(c("dplyr", "tidyverse", "lubridate", "usethis", "gitcreds", 
+                   "here", "medicaldata", "cowplot", "readr", "ggplot2" ))
 
-library("dplyr")
-library("tidyverse")
-library("lubridate")
+library(dplyr)
+library(tidyverse)
+library(lubridate)
+library(usethis)
+library(gitcreds)
+library(here)
+library(medicaldata)
+library(cowplot)
+library(readr)
+library(ggplot2)
+
 
---
+++ R/AMR_descriptive_statistics.R
@@ -0,0 +1,2 @@
+# This script is used for some first descriptive statistics with the datasets
+
