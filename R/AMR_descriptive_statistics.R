302be38c523939354b614a336570c2c422c808d5
Adrian Minnig <adrian.minnig96@gmail.com>
2023-11-22 20:51:04 +0100

    further optimizing graphs
    creating new graphs and optimizing existing ones

--- R/AMR_descriptive_statistics.R
+++ R/AMR_descriptive_statistics.R
@@ -50,8 +50,9 @@ summary(samples_cow)
 tbl_summary(samples, by = age)
 
 
-# Plot with all antibiotics ordered by decreasing number of resistant isolates      
-samples %>% 
+# plot1: Plot with all antibiotics ordered by decreasing number of resistant isolates      
+plot1 <-
+  samples %>% 
   group_by(age) %>% 
   summarise(TET = sum(TET),
             AMP = sum(AMP),
@@ -72,55 +73,120 @@ samples %>%
   slice(3:n(), 1:2) %>% 
   melt() %>%
   ggplot(aes(x = variable, y = (value / length(samples$Nr)), 
-  fill = reorder(age, -value), label = (value / length(samples$Nr)*100))) +
-  geom_bar(position="dodge", stat="identity") +
+             fill = reorder(age, -value), 
+             label = ifelse(variable == "TET" | 
+                              variable == "AMP" |
+                              variable == "SMX" |
+                              variable == "TMP" |
+                              variable == "CHL" |
+                              variable == "GEN" |
+                              variable == "CIP" |
+                              variable == "NAL" |
+                              variable == "AZI" |
+                              variable == "FOT" ,(value / length(samples$Nr)*100),""))) +
+  geom_bar(position="dodge", stat="identity", width = 0.8) +
   geom_text(aes(), position = position_dodge(0.8), vjust = -0.3,check_overlap = TRUE)+
   scale_fill_manual(values = c(hcl.colors(4, "Cividis")), labels = c('Calf Samples', 'Cow Samples')) +
   theme_bw() +
-  theme(plot.title = element_text(size = 30, hjust = 0.5, vjust = 0.5 ),legend.title=element_blank(),
-  axis.text=element_text(color="black"))+
+  theme(plot.title = element_text(size = 30, hjust = 0.5, vjust = 0.5 ),
+        legend.title = element_blank(), axis.title.y = element_text(size = 20,margin =margin(r=20)),
+        axis.text = element_text(color = "black"),
+        panel.grid.major = element_line(linewidth = 0.5, color = "lightgrey", linetype = "dashed"),
+        axis.ticks.x = element_line(linewidth = 20)) +
   xlab("") +
   ylab("Resistance Rate") + 
-  scale_y_continuous(labels = scales::percent) +
+  scale_y_continuous(labels = scales::percent, expand = c(0.01,0), 
+                     limits = c(0,0.1)) +
+  scale_x_discrete(expand = c(0,0.5)) +
   ggtitle("Resistance Rate of Antibiotics tested")
 
+print(plot1)
 
-# Plot with resistance rate by antibiotic class
+
+# plot2: Plot with resistance rate by antibiotic class
+plot2 <-
 samples %>% 
   group_by(age) %>% 
-  summarise(TET = sum(TET),
-            AMP = sum(AMP),
-            SMX = sum(SMX),
-            TMP = sum(TMP),
-            CHL = sum(CHL),
-            GEN = sum(GEN),
-            CIP = sum(CIP),
-            NAL = sum(NAL),
-            AZI = sum(AZI),
-            FOT = sum(FOT),
-            AMI = sum(AMI),
-            TGC = sum(TGC),
-            TAZ = sum(TAZ),
-            COL = sum(COL),
-            MERO = sum(MERO)) %>%
+  summarise(Tetracyclines = sum(TET),
+            Penicillins = sum(AMP),
+            Sulfonamides = sum(SMX),
+            Aminopyrimidines = sum(TMP),
+            Phenicoles = sum(CHL),
+            Aminoglycosides = sum(GEN,AMI),
+            Quinolones = sum(CIP,NAL),
+            Macrolides = sum(AZI),
+            Cephalosporins = sum(FOT,TAZ),
+            Glycylcyclines = sum(TGC),
+            Polypeptides = sum(COL),
+            Carbapenemns = sum(MERO)) %>%
   #janitor::adorn_totals(name = "All age groups") %>% 
   slice(3:n(), 1:2) %>% 
   melt() %>%
   ggplot(aes(x = variable, y = (value / length(samples$Nr)), 
-             fill = reorder(age, -value), label = (value / length(samples$Nr)*100))) +
-  geom_bar(position="dodge", stat="identity") +
+             fill = reorder(age, -value), 
+             label = ifelse(variable == "Penicillins" | 
+                            variable == "Tetracyclines" |
+                            variable == "Sulfonamides" |
+                            variable == "Aminopyrimidines" |
+                            variable == "Phenicoles" |
+                            variable == "Aminoglycosides" |
+                            variable == "Quinolones" |
+                            variable == "Macrolides" |
+                            variable == "Cephalosporins",
+                            (value / length(samples$Nr)*100),""))) +
+  geom_bar(position="dodge", stat="identity", width = 0.8) +
   geom_text(aes(), position = position_dodge(0.8), vjust = -0.3,check_overlap = TRUE)+
   scale_fill_manual(values = c(hcl.colors(4, "Cividis")), labels = c('Calf Samples', 'Cow Samples')) +
   theme_bw() +
-  theme(plot.title = element_text(size = 30, hjust = 0.5, vjust = 0.5 ),legend.title=element_blank()) +
+  theme(plot.title = element_text(size = 30, hjust = 0.5, vjust = 0.5 ),
+        legend.title = element_blank(), axis.title.y = element_text(size = 20,margin =margin(r=20)),
+        axis.text = element_text(color = "black"),
+        axis.text.x = element_text(angle = 45, hjust = 1, family = "sans", size = 10),
+        panel.grid.major = element_line(linewidth = 0.5, color = "lightgrey", linetype = "dashed"),
+        axis.ticks.x = element_line(linewidth = 24)) +
   xlab("") +
   ylab("Resistance Rate") + 
-  scale_y_continuous(labels = scales::percent) +
-  ggtitle("Resistance Rate of Antibiotics tested")
+  scale_y_continuous(labels = scales::percent, expand = c(0.01,0), 
+                     limits = c(0,0.1)) +
+  scale_x_discrete(expand = c(0,0.5)) +
+  ggtitle("Resistance Rate against Antimicrobial Classes")
+
+print(plot2)
 
 
 
 
+samples %>%
+  group_by(age) %>%
+  summarise('Pan-susceptible' = sum(class_res == 0),
+            'Resistances against 1-2 Classes ' = sum(class_res == 1 | class_res == 2),
+            'Multiclass resistant (≥ 3 Classes)' = sum(class_res >= 3)) %>%
+  slice(3:n(), 1:2) %>% 
+  melt() %>%
+  ggplot(aes(x = variable, y = (value / length(samples$Nr)), 
+             fill = reorder(age, -value), 
+             label = value)) +
+  geom_bar(position="dodge", stat="identity", width = 0.8) +
+  geom_text(aes(label = value), position = position_dodge(0.8), vjust = -0.3,check_overlap = TRUE)+
+  scale_fill_manual(values = c(hcl.colors(4, "Cividis")), labels = c('Calf Samples', 'Cow Samples')) +
+  theme_bw() +
+  theme(plot.title = element_text(size = 30, hjust = 0.5, vjust = 0.5 ),
+        legend.title = element_blank(), axis.title.y = element_text(size = 20,margin =margin(r=20)),
+        legend.text = element_text(size = 15),
+        axis.text = element_text(color = "black"),
+        axis.text.x = element_text(#angle = 45, hjust = 1, 
+                                   family = "sans", size = 15),
+        panel.grid.major = element_line(linewidth = 0.5, color = "lightgrey", linetype = "dashed")) +
+  xlab("") +
+  ylab("Resistance Rate") + 
+  scale_y_continuous(labels = scales::percent, expand = c(0.01,0), 
+                     limits = c(0,0.75), breaks = c(0.05,0.1,0.2,0.5,0.6,0.7,0.8),) +
+  scale_x_discrete(expand = c(0.21,0)) +
+  ggtitle("Occurence of (Multi)Class Resistances")
+
+
+          
+         
 
 
 
