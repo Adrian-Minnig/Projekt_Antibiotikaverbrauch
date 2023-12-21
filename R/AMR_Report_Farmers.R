d339d422b91d9bfacc5cbcf2a4cf47ad39e36741
Adrian Minnig <adrian.minnig96@gmail.com>
2023-12-21 17:13:36 +0100

    Create AMR_Report_Farmers.R
    Script to create figures for the communication of AMR results to farmers and veterinarians.

---
+++ R/AMR_Report_Farmers.R
@@ -0,0 +1,5 @@
+# This Script is used for generating plots for communication of fecal sample
+# results to farmers and veterinarians.
+
+# First Step: Recreate plot: resistant isolates in calves vs. cows by antibiotic
+# classes here with the correct proportions!
